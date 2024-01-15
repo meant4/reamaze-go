4dce4da141ff7122850d92c735d5043134100a18
Maciej Perlinski <maciej.perlinski@meant4.com>
2024-01-15 20:21:03 +0100

    adding Response Templates and Response Templates models

---
+++ reamaze/response_templates.go
@@ -0,0 +1,7 @@
+package reamaze
+
+// TODO
+// GET /response_templates https://www.reamaze.com/api/get_response_templates
+// GET /response_templates/{id} https://www.reamaze.com/api/get_response_template
+// POST /response_templates https://www.reamaze.com/api/post_response_template
+// PUT /response_templates/{slug} https://www.reamaze.com/api/put_response_template
