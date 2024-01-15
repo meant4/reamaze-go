aedff6052c5122fd9ca99aee0801a1dc3456daa8
Maciej Perlinski <maciej.perlinski@meant4.com>
2024-01-15 18:51:39 +0100

    Added GetArticles with respective models

--- reamaze/articles.go
+++ reamaze/articles.go
@@ -1,1 +1,26 @@
 package reamaze
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// GetArticles will allow you to retrieve Help Articles for the Brand
+// optional parameters WithArticlePage(int), WithArticleStatus(ReamazeArticleStatus),WithArticleQuery(string)
+// https://www.reamaze.com/api/get_articles
+func (c *Client) GetArticles(o ...ArticlesOption) (*GetArticlesResponse, error) {
+	var response *GetArticlesResponse
+	settings, _ := newArticlesSettings(o)
+	urlEndpoint := articlesEndpoint + settings.GetQuery()
+
+	resp, err := c.reamazeRequest(http.MethodGet, urlEndpoint, []byte{})
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(resp, &response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
